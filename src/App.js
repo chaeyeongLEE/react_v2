52451eaa88844dd3fa4467f9b3d72bb59f7acdc9
chaeyeongLEE <dh503401@naver.com>
2024-11-07 11:06:10 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          초기 설계
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
